bee5deee78e793566892e8714b4098af730da648
Richard Johnson Addai <72205808+Richard-Strive@users.noreply.github.com>
2022-11-15 15:38:57 +0100

    lil test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,32 +1,46 @@
 const express = require("express");
-const req = require("express/lib/request");
-const getHistoryPurchase = require("./product.js");
-
+const JSZip = require('jszip');
+const fs = require('fs');
 const route = express.Router();
 
 // PERSONAL PROFILE INFOS
-route.get("/search/city", (req, res) => {
+route.get("/csv", (req, res) => {
   try {
-    const product = getHistoryPurchase();
-    console.log("This is the product--->",product)
-    res.status(200).send(product);
-  } catch (error) {
-    console.log(error);
-  }
+    const csvData = fs.readFileSync('sample.csv');
+
+    zip.file("CSVFile.csv", csvData);
+
+    zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+        .pipe(fs.createWriteStream('sample.zip'))
+        .on('finish', function () {
+            console.log("sample.zip written.");
+        });
+
+    const data = zip.toBuffer();
+
+    const file_after_download = 'the_forksample.zip';
+
+    res.set('Content-Type','application/octet-stream');
+    res.set('Content-Disposition',`attachment; filename=${file_after_download}`);
+    res.set('Content-Length',data.length);
+    res.send(data);
+} catch (err) {
+    console.error(err)
+}
 });
 
-route.get("/search/city/:city", (req, res) => {
-  try {
-    const products = getHistoryPurchase();
-   console.log(req.params.city) 
+// route.get("/search/city/:city", (req, res) => {
+//   try {
+//     const products = getHistoryPurchase();
+//    console.log(req.params.city) 
 
- const finalProd= products.filter(product => product.localtion.toUpperCase() == req.params.city.toUpperCase() )
+//  const finalProd= products.filter(product => product.localtion.toUpperCase() == req.params.city.toUpperCase() )
 
-    res.status(200).send(finalProd);
-  } catch (error) {
-    console.log(error);
-  }
-});
+//     res.status(200).send(finalProd);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 /*route.get("/search", (req, res) => {
   try {
